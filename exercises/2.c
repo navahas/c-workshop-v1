8f00edb586765fccc6deb1b942dff3086298d65e
Carlos Navajas <c.navajas4@gmail.com>
2025-06-24 18:03:40 +0200

    feat: 2.c

--- exercises/2.c
+++ exercises/2.c
@@ -1,28 +1,17 @@
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 
-// 👉 First, build and run the program.
-//
-// To do this, make sure you're in the `exercises` directory, and then run:
-//
-// gcc -o app2 2.c && ./app2
-
 int main() {
-    char *header = "HTTP/1.1 200 OK";
+    char *header = "HTT\0P/1.1 200 OK";
 
-    // 👉 Try replacing this `15` with a call to `strlen` (and `#include <string.h>` above!)
-    write(1, header, 15);
-
-    // 👉 After you're using `strlen` above, try adding a `\0` (with the backslash)
-    // inside the definition of `header`, e.g.  "HTT\0P/1.1 200 OK" - this inserts
-    // a zero byte in the string. Before you run the program, what do you think it will print?
+    write(1, header, strlen(header));
+    printf("\nheader: %s", header);
 
     // 👉 Try changing this `%s` to `%zu` (ignore the compiler warning).
     printf("\n\nThat output was from write(). This is from printf: %s\n", header);
-
-    // 👉 Try changing the `%zud` back to `%s` and then replace the printf call's
-    // last argument (originally `header`) with this argument instead: `(char*)123456`
-    // 👉 Then try it with the number 0 instead of 123456.
+    printf("\n\nThat output was from write(). This is from printf: %zu\n", header);
+    printf("\n\nThat output was from write(). This is from printf: %zu\n", (char*)123456);
 
     return 0;
 }
